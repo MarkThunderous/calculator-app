903f6abcc56b4df9ec04054f5ab17e7bdb5c951a
MarkThunderous <mkhoroshylov2@gmail.com>
2023-10-27 17:08:59 +0300

    comment "test 2"

---
+++ tests/calculator.js
@@ -0,0 +1,57 @@
+let display = document.getElementById("display");
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Помилка";
+    }
+}
+
+module.exports = {
+    appendToDisplay,
+    clearDisplay,
+    calculateResult
+};
+
+// calculator.tests.js
+const { appendToDisplay, clearDisplay, calculateResult } = require('./calculator');
+
+describe('Calculator Functions', () => {
+    let display;
+
+    beforeEach(() => {
+        display = { value: '' };
+    });
+
+    test('should append a value to the display', () => {
+        appendToDisplay('5');
+        expect(display.value).toBe('5');
+    });
+
+    test('should clear the display', () => {
+        display.value = '123';
+        clearDisplay();
+        expect(display.value).toBe('');
+    });
+
+    test('should calculate the result', () => {
+        display.value = '2 + 3';
+        calculateResult();
+        expect(display.value).toBe('5');
+    });
+
+    test('should handle errors', () => {
+        display.value = '2 / 0';
+        calculateResult();
+        expect(display.value).toBe('Помилка');
+    });
+});
